8137b59917c003f1b62e12f338533a757273250e
Choco1125 <sebascruz981125@gmail.com>
2022-08-13 22:34:53 -0500

    feat ✨: Define routes on router file

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { faker } = require('@faker-js/faker');
+const routerApi = require('./routes/index');
 
 const app = express();
 const port = 3000;
@@ -13,51 +13,6 @@ app.get('/nueva-ruta', (req, res) => {
   res.send("Hi, I'm a new route");
 });
 
-//Route returning json
-app.get('/products', (req, res) => {
-  const products = [];
-
-  const { size } = req.query;
-
-  const limit = size || 10;
-
-  for (let i = 0; i < limit; i++) {
-    products.push({
-      name: faker.commerce.productName(),
-      price: parseInt(faker.commerce.price()),
-      image: faker.image.imageUrl(),
-    });
-  }
-
-  res.json(products);
-});
-
-//Route with paramas
-
-//Specific routes are firts and dinamic after
-app.get('/products/filter', (req, res) => {
-  res.send("I'm a filter");
-});
-
-app.get('/products/:id', (req, res) => {
-  const { id } = req.params;
-  res.json({
-    id,
-    name: 'Product 1',
-    price: 1000,
-  });
-});
-
-app.get('/categories/:categoryId/products/:productId', (req, res) => {
-  const { categoryId, productId } = req.params;
-  res.json({
-    categoryId,
-    productId,
-    name: 'Product 1',
-    price: 1000,
-  });
-});
-
 //Route with query params
 app.get('/users', (req, res) => {
   const { limit, offset } = req.query;
@@ -66,6 +21,8 @@ app.get('/users', (req, res) => {
 
   res.json({ limit, offset });
 });
+
+routerApi(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
---
+++ routes/products.routes.js
@@ -0,0 +1,51 @@
+const { Router } = require('express');
+const { faker } = require('@faker-js/faker');
+
+const router = Router();
+
+//Route returning json
+router.get('/', (req, res) => {
+  const products = [];
+
+  const { size } = req.query;
+
+  const limit = size || 10;
+
+  for (let i = 0; i < limit; i++) {
+    products.push({
+      name: faker.commerce.productName(),
+      price: parseInt(faker.commerce.price()),
+      image: faker.image.imageUrl(),
+    });
+  }
+
+  res.json(products);
+});
+
+//Route with paramas
+
+//Specific routes are firts and dinamic after
+router.get('/filter', (req, res) => {
+  res.send("I'm a filter");
+});
+
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  res.json({
+    id,
+    name: 'Product 1',
+    price: 1000,
+  });
+});
+
+router.get('/categories/:categoryId/products/:productId', (req, res) => {
+  const { categoryId, productId } = req.params;
+  res.json({
+    categoryId,
+    productId,
+    name: 'Product 1',
+    price: 1000,
+  });
+});
+
+module.exports = router;
