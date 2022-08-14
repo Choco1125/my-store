e9fb1314fdbe2b7c16a0ca639fe0a15ef2507e4b
Choco1125 <sebascruz981125@gmail.com>
2022-08-13 22:40:43 -0500

    feat ✨: Create route versioning

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,11 @@
+const { Router } = require('express');
 const productsRouter = require('./products.routes');
 
 function routerApi(app) {
-  app.use('/products', productsRouter);
+  const router = Router();
+
+  app.use('/api/v1', router);
+  router.use('/products', productsRouter);
 }
 
 module.exports = routerApi;
