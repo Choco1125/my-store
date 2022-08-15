070286f288cfdde94d573d5f0b0e1fa6b11d3333
Choco1125 <sebascruz981125@gmail.com>
2022-08-14 21:12:23 -0500

    feat 🎨: Apply changes from the course repo

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,17 @@
 const { Router } = require('express');
 const productsRouter = require('./products.routes');
+const categoriesRouter = require('./categories.routes');
+const usersRouter = require('./users.routes');
+const orderRouter = require('./orders.routes');
 
 function routerApi(app) {
   const router = Router();
 
   app.use('/api/v1', router);
   router.use('/products', productsRouter);
+  router.use('/categories', categoriesRouter);
+  router.use('/users', usersRouter);
+  router.use('/orders', orderRouter);
 }
 
 module.exports = routerApi;
