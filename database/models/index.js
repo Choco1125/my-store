33ad3673f5afec875219150b021e76beb4591be6
Choco1125 <sebascruz981125@gmail.com>
2022-10-17 16:54:43 -0500

    feat 🎨: Create customer and 1 to 1 relationship

--- database/models/index.js
+++ database/models/index.js
@@ -1,7 +1,10 @@
 const { User, userSchema } = require('./user.model');
+const { Customer, CustomerSchema } = require('./customer.model');
 
 function setupModels(sequelize) {
   User.init(userSchema, User.config(sequelize));
+  Customer.init(CustomerSchema, Customer.config(sequelize));
+  Customer.associate(sequelize.models);
 }
 
 module.exports = setupModels;
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const productsRouter = require('./products.routes');
 const categoriesRouter = require('./categories.routes');
 const usersRouter = require('./users.routes');
 const orderRouter = require('./orders.routes');
+const customersRoter = require('./customer.routes');
 
 function routerApi(app) {
   const router = Router();
@@ -12,6 +13,7 @@ function routerApi(app) {
   router.use('/categories', categoriesRouter);
   router.use('/users', usersRouter);
   router.use('/orders', orderRouter);
+  router.use('/customers', customersRoter);
 }
 
 module.exports = routerApi;
