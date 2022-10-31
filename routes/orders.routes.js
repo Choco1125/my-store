39d7b23568db9aec02c2e8ecd3057546771b5d7e
Choco1125 <sebascruz981125@gmail.com>
2022-10-30 22:23:24 -0500

    feat: :sparkles: Adding Order (POST, GET :id)

--- routes/orders.routes.js
+++ routes/orders.routes.js
@@ -1,9 +1,49 @@
 const express = require('express');
 
+const OrderService = require('./../services/order.service');
+const validatorHandler = require('./../middlewares/validator.handler');
+const {
+  createOrderSchema,
+  getOrderSchema,
+} = require('./../schemas/order.schema');
+
+const service = new OrderService();
+
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.json([]);
-});
+router.get(
+  '/:id',
+  validatorHandler(getOrderSchema, 'params'),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const order = await service.findOne(id);
+
+      res.status(200).json({
+        order,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+router.post(
+  '/',
+  validatorHandler(createOrderSchema, 'body'),
+  async (req, res, next) => {
+    try {
+      const body = req.body;
+      const order = await service.create(body);
+
+      res.status(201).json({
+        message: 'created',
+        data: order,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 
 module.exports = router;
