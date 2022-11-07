98cafb309291b8869e87a769ea9921cf2ab7e46c
Choco1125 <sebascruz981125@gmail.com>
2022-11-06 20:23:14 -0500

    feat: :lock: Apply jwt to protect routes

--- utils/auth/index.js
+++ utils/auth/index.js
@@ -1,3 +1,6 @@
 const passport = require('passport');
 const localStrategy = require('./strategies/local.stategy');
+const jwtStrategy = require('./strategies/jwt.strategy');
+
 passport.use(localStrategy);
+passport.use(jwtStrategy);
