a72ef1fc51231257a03affafcfe7e27b04732113
Choco1125 <sebascruz981125@gmail.com>
2022-11-06 21:39:42 -0500

    fix: :bug: Fixing total on create order

--- database/models/order.model.js
+++ database/models/order.model.js
@@ -30,6 +30,7 @@ const OrderSchema = {
   total: {
     type: DataTypes.VIRTUAL,
     get() {
+      if (!this.items) return 0;
       if (this.items.length === 0) return 0;
       return this.items.reduce(
         (total, item) => total + item.price * item.OrderProduct.ammount,
