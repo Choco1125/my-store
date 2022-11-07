4f51c5ba0ae113df472c2b74546e4b2e4e6ce1bd
Choco1125 <sebascruz981125@gmail.com>
2022-11-06 23:58:53 -0500

    refactor(Auth): :necktie: Applying service to local stategy

--- utils/auth/strategies/local.stategy.js
+++ utils/auth/strategies/local.stategy.js
@@ -1,21 +1,14 @@
 const { Strategy } = require('passport-local');
-const UserService = require('./../../../services/users.service');
-const boom = require('@hapi/boom');
-const bcrypt = require('bcrypt');
+const AuthService = require('./../../../services/auth.services');
 
-const service = new UserService();
+const service = new AuthService();
 
 const localStrategy = new Strategy(
   { usernameField: 'email', passwordField: 'password' },
   async (email, password, done) => {
     try {
-      const user = await service.findByEmail(email);
-      if (!user) done(boom.unauthorized(), false);
+      const user = await service.getUser(email);
 
-      const match = await bcrypt.compare(password, user.password);
-      if (!match) done(boom.unauthorized(), false);
-
-      delete user.dataValues.password;
       done(null, user);
     } catch (error) {
       done(error, false);
